8c960ec633198f12a0d77095c24c9aeac9f7fdb8
Jitesh Tukadiya <jiteshtukadiya@gmail.com>
2016-08-17 20:05:16 +0530

    bower added

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/api', api);
 app.use('/', routes);
