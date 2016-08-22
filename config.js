1590958f162ca6d909d090a483115976acb8026b
Jitesh Tukadiya <jiteshtukadiya@gmail.com>
2016-08-22 11:39:40 -0700

    twitter credentials changed

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     twitter: {
-        consumerKey: '79TG1w4Wp3lpL8mEuglfrClRd',
-        consumerSecret: 'SkagN4NKebwj4vPOjgHMvQgjLq8gzn8FrYqP0eIp4PgdGQvqA0',
+        consumerKey: 'qofW2XivgfxeOpP4fcuI3A0VJ',//79TG1w4Wp3lpL8mEuglfrClRd',
+        consumerSecret: 'zN61QnIuoqxgKDGj7JRQXkXPp3Qtrq62WC8aVP8ZzRE0Z4erih',
         callback: '/api/twitter/callback'
     },
     host: '0.0.0.0',
