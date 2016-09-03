2bf1b4b89b81f263064eb148404a9c06047f50c6
raj <rajtukadia1@gmail.com>
2016-09-04 02:50:54 +0530

    user with proxy

--- controllers/users.js
+++ controllers/users.js
@@ -7,24 +7,24 @@ module.exports = function() {
     return {
         login: function(req, res) {
 
-            var reqst = request({
-                method: req.method,
-                uri: req.app.locals.apiUrls.user + req.url,
-                body: req.body,
-                json: true
-            });
-
             // var reqst = request({
             //     method: req.method,
-			// 	headers: {
-			// 		header: 'Host:' + req.app.locals.apiUrls.userHost
-			// 		// header: 'Host:' + req.headers.host
-			// 	},
-            //     uri: req.app.locals.apiUrls.proxy + '/api' + req.url,
-            //     // uri: req.app.locals.apiUrls.pgEvent + req.url,
+            //     uri: req.app.locals.apiUrls.user + req.url,
             //     body: req.body,
             //     json: true
             // });
+
+            var reqst = request({
+                method: req.method,
+				headers: {
+					header: 'Host:' + req.app.locals.apiUrls.userHost
+					// header: 'Host:' + req.headers.host
+				},
+                uri: req.app.locals.apiUrls.proxy + '/api' + req.url,
+                // uri: req.app.locals.apiUrls.pgEvent + req.url,
+                body: req.body,
+                json: true
+            });
             
             reqst.pipe(res);
         }
